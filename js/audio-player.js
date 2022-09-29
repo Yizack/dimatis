70d329008b9b558dfcdc80796942b92882706225
Yizack Rangel <yizackr@gmail.com>
2022-09-28 19:10:26 -0500

    detect player color

--- js/audio-player.js
+++ js/audio-player.js
@@ -1,13 +1,25 @@
-(function() {
-    let media = document.querySelector("audio.fc-media");
-    let audio = new MediaElementPlayer(media, {
-        iconSprite: "",
-        audioHeight: 40,
-        features : ["playpause", "current", "duration", "progress", "volume", "tracks", "fullscreen"],
-        alwaysShowControls: true,
-        timeAndDurationSeparator: "<span></span>",
-        iPadUseNativeControls: false,
-        iPhoneUseNativeControls: false,
-        AndroidUseNativeControls: false
-    });
- })();
+let media = document.querySelector("audio.fc-media");
+let audio = new MediaElementPlayer(media, {
+    iconSprite: "",
+    audioHeight: 40,
+    features : ["playpause", "current", "duration", "progress", "volume", "tracks", "fullscreen"],
+    alwaysShowControls: true,
+    timeAndDurationSeparator: "<span></span>",
+    iPadUseNativeControls: false,
+    iPhoneUseNativeControls: false,
+    AndroidUseNativeControls: false
+});
+
+let img = new Image();
+img.crossOrigin = "Anonymous";
+
+let imageElement = document.getElementById("image");
+img.src = imageElement.style.backgroundImage.replace('url("', "").replace('")', "");
+
+img.onload = () => {
+    let colorThief = new ColorThief();
+    let color = colorThief.getColor(img);
+    let player = document.getElementById("player");
+    player.style.backgroundColor = "rgb(" + color + ")";
+    player.style.display = "block";
+}
