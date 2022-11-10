2e6bbe01309e0ef7871b5a4261770ce25af7aaf8
Yizack Rangel <yizackr@gmail.com>
2022-11-09 23:00:41 -0500

    Skip already existing
    This is useful for album tracks having the same cover art

--- players.style.js
+++ players.style.js
@@ -5,17 +5,26 @@ import ColorThief from "colorthief";
 
 const require = createRequire(import.meta.url);
 const tracks = require("./public/data/tracks.json");
-let contents = "";
+let styles = {};
+let css_content = "";
 
 Object.keys(tracks).forEach((param) => {
-  const img = resolve(process.cwd(), `./public/images/${tracks[param]?.cover ? tracks[param].cover : param}.jpg`);
+  const filename = tracks[param]?.cover ? tracks[param].cover : param;
+  const img = resolve(process.cwd(), `./public/images/${filename}.jpg`);
   ColorThief.getColor(img).then(color => {
-    contents += `.${param} {\n  background-color: rgb(${color[0]}, ${color[1]}, ${color[2]});\n}\n`;
+    if (!styles?.[filename]) {
+      styles[filename] = {
+        color: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+      };
+      css_content += `.${filename} {\n  background-color: ${styles[filename].color};\n}\n`;
+      writeFile("./assets/css/player-styles.css", css_content, "utf-8", () => {
+        console.log(`Generated style: ${filename}`);
+      });
+    }
+    else {
+      console.log(`Already exists: ${filename}, skipping`);
+    }
   }).catch(err => {
     console.log(err);
-  }).finally(() => {
-    writeFile("./assets/css/player-styles.css", contents, "utf-8", () => {
-      console.log(`Generated style: ${param}`);
-    });
   });
 });
