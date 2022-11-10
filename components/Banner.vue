df9edffb1a5dbf6384bafdb6f2fca62e26318069
Yizack Rangel <yizackr@gmail.com>
2022-11-10 00:33:00 -0500

    Logo alt

--- components/Banner.vue
+++ components/Banner.vue
@@ -5,7 +5,7 @@ import { SITE } from "~/site-info.js";
 <template>
   <section id="banner">
     <div class="text-center py-5">
-      <img class="img-fluid rounded-circle" :src="SITE.logo" width="150" height="150">
+      <img class="img-fluid rounded-circle" :src="SITE.logo" width="150" height="150" alt="Dimatis Logo">
       <h1>{{ SITE.name }}</h1>
       <p class="mb-0">{{ SITE.description }}</p>
     </div>
