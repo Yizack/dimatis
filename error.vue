812abe39aa94d6e6700e26e28c0d4d3d51a3df44
Yizack Rangel <yizackr@gmail.com>
2023-07-19 04:20:54 -0500

    chore: add container to error page

--- error.vue
+++ error.vue
@@ -3,7 +3,7 @@ const { statusCode, message } = useError().value;
 </script>
 
 <template>
-  <div class="vh-100 d-flex justify-content-center align-items-center">
+  <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div>
       <h1>Error {{ statusCode }}</h1>
       <h5>Sorry, an error has occured. {{ message }}</h5>
