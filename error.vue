71675dd87106c17e9d7d5ac4b4aefbbbb64cc73b
Yizack Rangel <yizackr@gmail.com>
2023-07-19 03:53:40 -0500

    chore: error page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { statusCode, message } = useError().value;
+</script>
+
+<template>
+  <div class="vh-100 d-flex justify-content-center align-items-center">
+    <div>
+      <h1>Error {{ statusCode }}</h1>
+      <h5>{{ message }}</h5>
+    </div>
+  </div>
+</template>
