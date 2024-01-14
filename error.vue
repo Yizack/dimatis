d4ad68d30dd5652f78169fdd855b5b4a41c1f408
Yizack Rangel <yizackr@gmail.com>
2024-01-14 10:52:22 -0500

    chore: updates

--- error.vue
+++ error.vue
@@ -1,12 +1,18 @@
 <script setup>
-const { statusCode, message } = useError().value;
+const error = useError();
+
+useHead({
+  bodyAttrs: {
+    class: "bg-dark"
+  }
+});
 </script>
 
 <template>
   <div class="container vh-100 d-flex justify-content-center align-items-center">
-    <div>
-      <h1>Error {{ statusCode }}</h1>
-      <h5>Sorry, an error has occured. {{ message }}</h5>
+    <div class="bg-light p-5 rounded-3 shadow text-dark">
+      <h1>Error {{ error.statusCode }}</h1>
+      <h5>Sorry, an error has occured. {{ error.message || error.statusMessage }}</h5>
       <p>Go back: <NuxtLink to="/" class="text-decoration-underline">dimatis.yizack.com</NuxtLink></p>
     </div>
   </div>
