7941e5731685d14272e360cd290e83ca314237cf
Yizack <yizackr@gmail.com>
2024-02-12 14:30:29 +0000

    Deploying to gh-pages from @ Yizack/dimatis@15fc43662385ef5af5defaddfba770575c37677b 🚀

---
+++ _nuxt/MusicfyPlayer.pgc-VXM0.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./MusicfyPlayer.vue.sljUe6xs.js";import"./entry.fvMEM_Rc.js";export{m as default};
---
+++ _nuxt/index.kDnixztv.js
@@ -0,0 +1,1 @@
+import{b as l,y as m}from"./entry.fvMEM_Rc.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return l({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=m({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
