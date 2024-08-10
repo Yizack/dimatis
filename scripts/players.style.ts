9cb4d993a9e251e8ec7b61c52b2d999ec4e92f2a
Yizack Rangel <yizackr@gmail.com>
2024-08-09 23:46:08 -0500

    fix: style generation script

--- scripts/players.style.ts
+++ scripts/players.style.ts
@@ -17,7 +17,7 @@ for (const param in tracks) {
         color: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
       };
       css_content += `.${/^\d/.test(filename) ? `\\3${filename}` : filename} { background-color: ${styles[filename].color}; }\n`;
-      writeFile("./assets/css/player-styles.css", css_content, "utf-8", () => {
+      writeFile("./app/assets/css/player-styles.css", css_content, "utf-8", () => {
         console.info(`Generated style: ${filename}`);
       });
     }
