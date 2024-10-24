9834248a158d14b00f2da97d2b783604f0e6d6a0
Yizack Rangel <yizackr@gmail.com>
2024-10-24 05:00:09 -0500

    chore: snake_case db

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,15 +1,8 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const downloads = sqliteTable("downloads", {
-  id: integer("id").primaryKey(),
-  file: text("file").notNull().unique(),
-  count: integer("count").notNull().default(0),
-  lastDownload: integer("last_download")
-});
-
-export const fanlinks = sqliteTable("fanlinks", {
-  id: integer("id").primaryKey(),
-  track: integer("track").notNull(),
-  service: text("service").notNull(),
-  link: text("link").notNull()
+  id: integer().primaryKey(),
+  file: text().notNull().unique(),
+  count: integer().notNull().default(0),
+  lastDownload: integer()
 });
