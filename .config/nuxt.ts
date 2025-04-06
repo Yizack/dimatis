3823a96c7d778e5dbf4b692d13b645ffed3096f9
Yizack Rangel <yizackr@gmail.com>
2025-04-06 06:07:19 -0500

    chore: do not preload fonts

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -19,8 +19,6 @@ export default defineNuxtConfig({
         { name: "msapplication-TileImage", content: "/icons/mstile-144x144.png" }
       ],
       link: [
-        { rel: "preload", href: "/fonts/Ebrima.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" },
-        { rel: "preload", href: "/fonts/EbrimaBd.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" },
         { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
         { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
         { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-chrome-192x192.png" },
