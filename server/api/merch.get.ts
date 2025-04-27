193ca69bdfd58397bfe5fa3085e04bbe1e6df93e
Yizack Rangel <yizackr@gmail.com>
2025-04-27 05:11:38 -0500

    chore: display specific products

--- server/api/merch.get.ts
+++ server/api/merch.get.ts
@@ -8,12 +8,19 @@ export default defineCachedEventHandler(async (event) => {
   }).catch(() => null);
 
   const products = response?.results || [];
+  const productIds = [
+    "755140be-5fb9-49b2-9123-efae7c4151f1",
+    "d65bdade-b919-44bc-9347-7929107f63cc",
+    "075ade71-6e32-4350-b115-e92687430cba",
+    "6a7535a0-c979-48f6-adbf-da2835e7a939",
+    "4677f66b-2702-4ceb-9095-25f5c8c71981",
+    "989d19cf-7637-4c5c-b24c-7b67dbd35f89",
+    "b58ee0a5-b201-4587-bc98-3945361e8b68",
+    "7e16dccb-55c5-42f4-a452-71f2730e9c5d"
+  ];
 
   return products.filter((product) => {
-    const isPublic = product.access.type === "PUBLIC";
-    const hasImages = product.images.length > 0;
-    const hasVariants = product.variants.length > 0;
-    return isPublic && hasImages && hasVariants;
+    return productIds.includes(product.id);
   }).map(product => ({
     id: product.id,
     name: product.name,
@@ -21,7 +28,11 @@ export default defineCachedEventHandler(async (event) => {
     slug: product.slug,
     image: product.images[0]!,
     price: product.variants[0]!.unitPrice.value.toFixed(2)
-  })).reverse().slice(0, 8);
+  })).sort((a, b) => {
+    const aIndex = productIds.indexOf(a.id);
+    const bIndex = productIds.indexOf(b.id);
+    return aIndex - bIndex;
+  });
 }, {
   swr: false,
   group: "api",
