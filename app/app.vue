d7876d915d04c88c5362a18c7764911aa63f402e
Yizack Rangel <yizackr@gmail.com>
2025-05-05 19:36:03 -0500

    refactor: set scroll behavior in app entry

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-setScrollBehavior();
+const { $router } = useNuxtApp();
+
+$router.options.scrollBehavior = to => new Promise((resolve) => {
+  if (!to.hash) return resolve({ left: 0, top: 0 });
+  setTimeout(() => resolve({ el: to.hash, top: 55, left: 0, behavior: "smooth" }), 500);
+});
 </script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-  <NuxtLoadingIndicator :throttle="0" />
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <NuxtLoadingIndicator :throttle="0" />
+  </div>
 </template>
 
-<style scoped>
+<style>
 .nuxt-loading-indicator {
   background: repeating-linear-gradient(to right,#3e61c5 0%,#3e61c5 50%,#bfd0ff 100%)!important;
 }
