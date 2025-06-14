64d62152de257afe9242bfa230a7e74251429aa2
Yizack Rangel <yizackr@gmail.com>
2025-06-14 06:42:02 -0500

    refactor(instagram): add feed limit option

--- server/api/instagram/feed.get.ts
+++ server/api/instagram/feed.get.ts
@@ -2,8 +2,11 @@ export default defineCachedEventHandler(async (event) => {
   const { instagram } = useRuntimeConfig(event);
   const API = new InstagramFeed(instagram.accessToken);
   await API.refreshAccessToken();
-  const feed = await API.getFeed(["permalink", "timestamp", "caption", "media_url", "media_type", "thumbnail_url", "is_shared_to_feed"]);
-  return feed.filter(post => post.is_shared_to_feed);
+  const feed = await API.getFeed({
+    fields: ["permalink", "timestamp", "caption", "media_url", "media_type", "thumbnail_url", "is_shared_to_feed"],
+    limit: 80
+  });
+  return feed.data.filter(post => post.is_shared_to_feed);
 }, {
   group: "api",
   name: "instagram",
--- server/utils/instagram.ts
+++ server/utils/instagram.ts
@@ -1,28 +1,30 @@
 type InstagramFields = "username" | "permalink" | "timestamp" | "caption" | "media_url" | "media_type" | "thumbnail_url" | "is_shared_to_feed";
 
 export class InstagramFeed {
-  api: string;
+  api: string = "https://graph.instagram.com";
+  version: string = "v23.0";
   accessToken: string;
   constructor (accessToken: string) {
-    this.api = "https://graph.instagram.com";
     this.accessToken = accessToken;
   }
 
   async refreshAccessToken () {
     const query = new URLSearchParams({ grant_type: "ig_refresh_token", access_token: this.accessToken });
-    const response = await $fetch(`${this.api}/refresh_access_token?${query}`).catch((e) => {
+    return $fetch(`${this.api}/refresh_access_token?${query}`).catch((e) => {
       console.warn(e);
       throw createError({ statusCode: ErrorCode.INTERNAL_SERVER_ERROR, message: "Failed to refresh access token" });
     });
-    return response;
   }
 
-  async getFeed (fields: InstagramFields[]) {
-    const query = new URLSearchParams({ fields: fields.join(), access_token: this.accessToken });
-    const response = await $fetch<{ data: InstagramPost[] }>(`${this.api}/me/media?${query}`).catch((e) => {
+  async getFeed (options: { fields: InstagramFields[], limit?: number }) {
+    const query = new URLSearchParams({
+      fields: options.fields.join(),
+      access_token: this.accessToken,
+      limit: options.limit?.toString() || "25"
+    });
+    return $fetch<{ data: InstagramPost[] }>(`${this.api}/${this.version}/me/media?${query}`).catch((e) => {
       console.warn(e);
       throw createError({ statusCode: ErrorCode.INTERNAL_SERVER_ERROR, message: "Failed to fetch instagram feed" });
     });
-    return response.data;
   }
 }
