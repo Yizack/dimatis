2b600e638f3eb73b8ae91d820febecac65c3c4cc
Yizack Rangel <yizackr@gmail.com>
2025-09-03 12:42:02 +0200

    chore: remove mixed-decls deprecation silence

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -133,7 +133,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          silenceDeprecations: ["mixed-decls", "color-functions", "import", "global-builtin"]
+          silenceDeprecations: ["color-functions", "import", "global-builtin"]
         }
       }
     }
