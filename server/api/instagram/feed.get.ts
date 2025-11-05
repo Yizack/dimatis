f8a0d607b25c7740b7f3fc1424bcd7180fa73b7d
Yizack Rangel <yizackr@gmail.com>
2025-11-05 01:23:31 -0500

    refactor: improve ig feed api code readability

--- server/api/instagram/feed.get.ts
+++ server/api/instagram/feed.get.ts
@@ -1,11 +1,14 @@
 export default defineCachedEventHandler(async (event) => {
   const { instagram } = useRuntimeConfig(event);
-  const API = new InstagramFeed(instagram.accessToken);
-  await API.refreshAccessToken();
-  const feed = await API.getFeed({
+
+  const instagramFeed = new InstagramFeed(instagram.accessToken);
+  await instagramFeed.refreshAccessToken();
+
+  const feed = await instagramFeed.getFeed({
     fields: ["permalink", "timestamp", "caption", "media_url", "media_type", "thumbnail_url", "is_shared_to_feed"],
     limit: 80
   });
+
   return feed.data.filter(post => post.is_shared_to_feed);
 }, {
   group: "api",
